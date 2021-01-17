0803bbc025fd8397efac44dd162c81b764efa515
vasc9380 <vasc9380@th-wildau.de>
2021-01-17 02:43:08 +0100

    Minor fixes in setup.

--- node/init.sh
+++ node/init.sh
@@ -8,15 +8,15 @@ echo "##########################################################################
 npm install pm2 -g
 
 # Install node packages
-npm install ~/software/node/rose-api/ 
-npm install ~/software/node/rose-frontend/ 
+npm install /home/rose/software/node/rose-api/ 
+npm install /home/rose/software/node/rose-frontend/ 
 
 echo "###########################################################################################"
 echo "Start node apps"
 echo "###########################################################################################"
 
-
 # Start services
-pm2 start ~/software/node/rose-api/api.js --watch -n rose-api
-pm2 start ~/software/node/rose-frontend/app.js --watch -n rose-frontend
+cd /home/rose/software/node/rose-api/ && pm2 start api.js --watch -n rose-api
+cd /home/rose/software/node/rose-frontend/ && pm2 start app.js --watch -n rose-frontend
 pm2 save
+
--- setup/installation.sh
+++ setup/installation.sh
@@ -12,7 +12,7 @@ now=$(date +%d-%m-%Y" "%H:%M:%S | sed -e "s/ /_/g")
 
 
 
-sudo apt update
+apt update
 apt -y upgrade 
 
 # Installation Basis Software #########################################################
@@ -21,12 +21,12 @@ echo "##########################################################################
 echo "Installation von Basis Software"
 echo "###########################################################################################"
 
-sudo apt install -y curl build-essential gdb gnupg2 lsb-release git 
+apt install -y curl build-essential gdb gnupg2 lsb-release git 
 
 # sudo apt install -y python-pip python-dev
-sudo apt install -y python3 python3-pip
-sudo apt install -y libssl-dev libffi-dev python-dev
-sudo apt install -y portaudio19-dev
+apt install -y python3 python3-pip
+apt install -y libssl-dev libffi-dev python-dev
+apt install -y portaudio19-dev
 
 
 pythonPackages=(redis numpy scipy nose opencv-python pyusb pyaudio pixel-ring argcomplete smbus) #matplotlib ipython jupyter pandas sympy
@@ -40,7 +40,7 @@ done
 echo "###########################################################################################"
 echo "Installation NGINX"
 echo "###########################################################################################"
-sudo apt install nginx
+apt install -y nginx
 
 # Setup nginx configuration
 cp -p /home/rose/software/setup/nginx-config.txt /etc/nginx/sites-enabled/default
@@ -53,20 +53,21 @@ echo "##########################################################################
 
 # Using Ubuntu
 # https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash - # For v15
+curl -sL https://deb.nodesource.com/setup_15.x | -E bash - # For v15
 # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -  # You need 12 for if you want to use ROS2 NodeJS https://github.com/RobotWebTools/rclnodejs
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # GCC if not installed for native addons
-sudo apt-get install gcc g++ make
+apt-get install -y gcc g++ make
 
 # Yarn for Node JS
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && sudo apt-get install yarn
 
 # Run node applications
-bash /home/rose/software/node/init.sh
+env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u rose --hp /home/rose
+sudo -u rose bash /home/rose/software/node/init.sh
 
 echo "###########################################################################################"
 echo "Installation ROS2"
@@ -75,16 +76,20 @@ echo "##########################################################################
 # https://index.ros.org/doc/ros2/Installation/Foxy/Linux-Install-Debians/
 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update
+sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+apt update
 # sudo apt install ros-foxy-desktop
-sudo apt install ros-foxy-ros-base #ROS-Base Install (Bare Bones): Communication libraries, message packages, command line tools. No GUI tools
-sudo apt install python3-colcon-common-extensions
+apt install -y ros-foxy-ros-base #ROS-Base Install (Bare Bones): Communication libraries, message packages, command line tools. No GUI tools
+apt install -y python3-colcon-common-extensions
 
 # Creating goto script for easier ROS2 Setup in home directory
-echo "source ~/software/ros2/ros_ws/install/setup.bash" > /home/rose/goto.sh
-echo "cd ~/software/ros2/ros_ws" >> /home/rose/goto.sh
-chmod u+x /home/rose/goto.sh
+sudo -u rose echo "source ~/software/ros2/ros_ws/install/setup.bash" > /home/rose/goto.sh
+sudo -u rose echo "cd ~/software/ros2/ros_ws" >> /home/rose/goto.sh
+sudo -u rose chmod u+x /home/rose/goto.sh
+
+# Build ROS2
+cd /home/rose/software/ros2/ros_ws/ && sudo -u rose colcon build
+
 
 # Installation Redis #########################################################
 
@@ -117,18 +122,18 @@ echo "##########################################################################
 echo "Configure I2C"
 echo "###########################################################################################"
 
-sudo apt-get install -y python3-smbus
-sudo apt-get install -y i2c-tools
-sudo apt install libi2c-dev
+apt-get install -y python3-smbus
+apt-get install -y i2c-tools
+apt install -y libi2c-dev
 
 # RasPi Config
 # https://askubuntu.com/questions/1273700/enable-spi-and-i2c-on-ubuntu-20-04-raspberry-pi
 wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20201108_all.deb -P /tmp
-sudo dpkg -i /tmp/raspi-config_20201108_all.deb
-sudo apt-get install lua5.1
-sudo apt-get install alsa-utils
-sudo apt --fix-broken install
-sudo dpkg -i /tmp/raspi-config_20201108_all.deb
-sudo raspi-config
+dpkg -i /tmp/raspi-config_20201108_all.deb
+apt-get install -y lua5.1
+apt-get install -y alsa-utils
+apt --fix-broken install
+dpkg -i /tmp/raspi-config_20201108_all.deb
+raspi-config
 # Run raspi-config and enable I2C 
 
