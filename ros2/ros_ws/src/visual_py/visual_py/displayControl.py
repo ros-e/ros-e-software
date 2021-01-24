a42bfaf62f6f924d3ed322a5c25030a3d59e7c04
Valentin Schroeter <vasc9380@th-wildau.de>
2021-01-24 02:25:44 +0000

    Updated DisplayControl Node to the new Eye-Methods.

--- ros2/ros_ws/src/visual_py/visual_py/displayControl.py
+++ ros2/ros_ws/src/visual_py/visual_py/displayControl.py
@@ -8,166 +8,316 @@ import time
 
 # ROS 2 Imports
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node, SrvTypeResponse
 
-from std_msgs.msg import String, Float32, UInt8, Int16, ColorRGBA
+from std_msgs.msg import String, Float32, UInt8, Int16, ColorRGBA, Bool
 from systemcore.msg import I2Cwrite8, I2Cwrite16, I2CwriteArray 
-from visual.msg import DisplayConnectedPixel, Point, DisplayPixel
+from visual.msg import Polygon, PolygonPoint # DisplayConnectedPixel, Point, DisplayPixel
 
-# class Commands():
-#   MOTOR_SET_STIFFNESS = 0x10
-#   MOTOR_TURN_SET_ABSOLUTE = 0x11
-#   MOTOR_TURN_SET_RELATIVE = 0x12
-#   MOTOR_PITCH_SET_ABSOLUTE = 0x13
-#   MOTOR_PITCH_SET_RELATIVE = 0x14
 
-#   LED_SET_RGB = 0x51
+def toUint8(x):
+    return x % 256
 
-# See: https://icampusnet.th-wildau.de/gitlab/ros-e/tischroboter-software-sbc/wikis/Arduino-I2C-Kommandos
-# DISPLAY_CLEAR        = 0x70
-# DISPLAY_SET_PIXEL    = 0x71
-# DISPLAY_SET_EYE      = 0x72
-# DISPLAY_EYE_DATA     = 0x73
-# DISPLAY_SET_IRIS     = 0x74
-# DISPLAY_IRIS_DATA    = 0x75
+def toUint16(x):
+    return x % (256 * 256) 
+
+def uint16ToBytes(x):
+    return [int(x >> 8), int(x & 0x00FF)]
+
+# See: https://icampusnet.th-wildau.de/gitlab/ros-e/tischroboter-doku/wikis/System/Arduino-I2C-Kommandos
+class Commands():
+
+    CMD_DISPLAY_CLEAR = 0x10
+    CMD_DISPLAY_SET_PIXEL = 0x11
+
+    CMD_DISPLAY_UINT8_100VI_EYE_SINGLE = 0x20
+    CMD_DISPLAY_UINT8_100VI_EYE_START_SEQ = 0x21
+    CMD_DISPLAY_UINT8_100VI_EYE_CONT_SEQ = 0x22
+    CMD_DISPLAY_UINT8_100VI_EYE_END_SEQ = 0x23
+
+    CMD_DISPLAY_UINT8_100VI_IRIS_SINGLE = 0x28
+    CMD_DISPLAY_UINT8_100VI_IRIS_START_SEQ = 0x29
+    CMD_DISPLAY_UINT8_100VI_IRIS_CONT_SEQ = 0x2A
+    CMD_DISPLAY_UINT8_100VI_IRIS_END_SEQ = 0x2B
+
+
+    CMD_DISPLAY_UINT8_CUSTOMVI_EYE_SINGLE = 0x30
+    CMD_DISPLAY_UINT8_CUSTOMVI_EYE_START_SEQ = 0x31
+    CMD_DISPLAY_UINT8_CUSTOMVI_EYE_CONT_SEQ = 0x32
+    CMD_DISPLAY_UINT8_CUSTOMVI_EYE_END_SEQ = 0x33
+
+    CMD_DISPLAY_UINT8_CUSTOMVI_IRIS_SINGLE = 0x38
+    CMD_DISPLAY_UINT8_CUSTOMVI_IRIS_START_SEQ = 0x39
+    CMD_DISPLAY_UINT8_CUSTOMVI_IRIS_CONT_SEQ = 0x3A
+    CMD_DISPLAY_UINT8_CUSTOMVI_IRIS_END_SEQ = 0x3B
+
+
+    CMD_DISPLAY_INT16_EYE_SINGLE = 0x40
+    CMD_DISPLAY_INT16_EYE_START_SEQ = 0x41
+    CMD_DISPLAY_INT16_EYE_CONT_SEQ = 0x42
+    CMD_DISPLAY_INT16_EYE_END_SEQ = 0x43
+
+    CMD_DISPLAY_INT16_IRIS_SINGLE = 0x48
+    CMD_DISPLAY_INT16_IRIS_START_SEQ = 0x49
+    CMD_DISPLAY_INT16_IRIS_CONT_SEQ = 0x4A
+    CMD_DISPLAY_INT16_IRIS_END_SEQ = 0x4B
+  
+    CMD_VIEW_DIRECTION = 0xA0
+    CMD_FILL_POLYGON = 0xA8
+
+#define CMD_VIEW_DIRECTION 0xA0
+
 
 i2cSem = threading.Semaphore()
 
+class Display():
+    def __init__(self, parentNode, i2cArrayPublisher, side, arduinoAddress):
+
+        self.arduinoI2C = arduinoAddress
+        self.side = side
+        self.fill = True
+
+        self.parentNode = parentNode
+        self.logger = self.parentNode.get_logger()
+
+        # Messages for set eye pixel
+        self.parentNode.create_subscription(Polygon, "visual/display/setEye", self.onSetEye, 10)
+        self.parentNode.create_subscription(Polygon, "visual/display/" + self.side + "/setEye", self.onSetEye, 10)
+
+        # Messages for set iris pixel
+        self.parentNode.create_subscription(Polygon, "visual/display/setIris", self.onSetIris, 10)
+        self.parentNode.create_subscription(Polygon, "visual/display/" + self.side + "/setIris", self.onSetIris, 10)
+
+        # Messages for set polygon fill
+        self.parentNode.create_subscription(Bool, "visual/display/setFill", self.onSetFill, 10)
+        self.parentNode.create_subscription(Polygon, "visual/display/" + self.side + "/setIris", self.onSetIris, 10)
+
+
+        # Messages for Display a single pixel on a Display
+        # self.create_subscription(DisplayPixel, "visual/display/setPixel", self.onSetPixel, 10)
+        # self.create_subscription(DisplayPixel, "visual/display/" + self.side + "/setPixel", self.onSetPixel, 10)
+
+        # Messages for clearing the displays
+        # self.create_subscription(UInt8, "visual/display/clear", self.onClearDisplay, 10)
+        # self.create_subscription(UInt8, "visual/display/" + self.side + "/clear", self.onClearDisplay, 10)
+
+        # self.pubI2Cwrite8 = self.parentNode.create_publisher(I2Cwrite8, "system/i2c/write8", 10)
+        # self.pubI2Cwrite16 = self.parentNode.create_publisher(I2Cwrite16, "system/i2c/write16", 10)
+        self.pubI2CwriteArray = i2cArrayPublisher
+
+        ################################
+        ### LOG INFO ###################
+        self.logger.info("################### Init {} display node ################### ".format(self.side))
+        self.logger.info("Subscribed: visual/display/setEye        |  Msg: visual/DisplayConnectedPixel")
+        self.logger.info("Subscribed: visual/display/{}/setEye  |  Msg: visual/DisplayConnectedPixel".format(self.side))
+
+        self.logger.info("Subscribed: visual/display/setIris       |  Msg: visual/DisplayConnectedPixel")
+        self.logger.info("Subscribed: visual/display/{}/setIris |  Msg: visual/DisplayConnectedPixel".format(self.side))
+
+        self.logger.info("Subscribed: visual/display/setPixel        |  Msg: visual/DisplayPixel")
+        self.logger.info("Subscribed: visual/display/{}/setPixel  |  Msg: visual/DisplayPixel".format(self.side))
+
+        self.logger.info("Subscribed: visual/display/clear         |  Msg: std_msgs/UInt8")
+        self.logger.info("Subscribed: visual/display/{}/clear   |  Msg: std_msgs/UInt8".format(self.side))
+
+        self.logger.info("--------------------------------------------------------------------------")
+        # self.logger.info("Publish on: system/i2c/write8      |  Msg: system/I2Cwrite8")
+        # self.logger.info("Publish on: system/i2c/write16     |  Msg: system/I2Cwrite16")
+        self.logger.info("Publish on: system/i2c/writeArray  |  Msg: system/I2CwriteArray")
+
+        self.logger.info("Started {} Display Control Node".format(self.side))
+
+    # def transmitPixelData(self, msg, cmdControlData, cmdData):
+    
+    #     i2cSem.acquire()
+        
+    #     time.sleep(0.01)    
+
+    #     # Transmit the control data
+    #     o = I2CwriteArray()
+    #     o.address = self.arduinoI2C
+    #     o.command = cmdControlData
+    #     o.data = [int(msg.curve), int(msg.animation), int(msg.value), int(len(msg.points))]
+
+    #     self.pubI2CwriteArray.publish(o)
+    #     time.sleep(0.01)
+
+    #     # Transmit the pixel data
+    #     o = I2CwriteArray()
+    #     o.address = self.arduinoI2C
+    #     o.command = cmdData
+
+    #     data = []
+
+    #     for point in msg.points:
+    #         data.append(int(point.row))
+    #         data.append(int(point.col))
+
+    #     o.data = data
+    #     self.pubI2CwriteArray.publish(o)
+
+    #     i2cSem.release()
+
+    def publish_i2c_array(self, cmd, data):
+        i2cSem.acquire()
+
+        o = I2CwriteArray()
+        o.address = self.arduinoI2C
+        o.command = cmd
+        o.data = data
+
+        self.pubI2CwriteArray.publish(o)
+        i2cSem.release()
+
+
+    def publishPoints(self, points, viewInfluence, seqNumber, finalSeq=True, type="eye"):
+
+        data = []
+
+        command = 0
+
+        is_uint8 = True
+        customVI = viewInfluence != 100
+
+        ### Check if uint8 data ###
+        for p in points:
+            if p.x < 0 or p.x > 255: is_uint8 = False
+            if p.y < 0 or p.y > 255: is_uint8 = False
+            if p.cx < 0 or p.cx > 255: is_uint8 = False
+            if p.cy < 0 or p.cy > 255: is_uint8 = False
+
+        if is_uint8:
+            if customVI:
+                command = Commands.CMD_DISPLAY_UINT8_100VI_EYE_SINGLE
+            else:
+                command = Commands.CMD_DISPLAY_UINT8_CUSTOMVI_EYE_SINGLE
+                data.append(viewInfluence)
+        else:
+            command = Commands.CMD_DISPLAY_INT16_EYE_SINGLE
+            data.append(viewInfluence)
+
+        if type == "iris":   # If eye cmd stay as it is, otherwise increase by 8
+            command += 0x08
+
+        if finalSeq:
+            if seqNumber == 0:      # single sequence
+                pass # No cmd increasind
+            else:                   # end sequences
+                command += 0x03
+        else:  # k of n sequences
+            if seqNumber == 0:      # beginning sequence
+                command += 0x01
+            else:                   # continuing sequence
+                command += 0x02
+
+        if is_uint8:
+            for p in points:
+                data.append(toUint8(p.x))
+                data.append(toUint8(p.y))
+                data.append(toUint8(p.c))
+                data.append(toUint8(p.cx))
+                data.append(toUint8(p.cy))
+        else:
+            for p in points:
+                data += uint16ToBytes(toUint16(p.x))
+                data += uint16ToBytes(toUint16(p.y))
+                data.append(toUint8(p.c))
+                data += uint16ToBytes(toUint16(p.cx))
+                data += uint16ToBytes(toUint16(p.cy))
+
+        self.publish_i2c_array(command, data)
+
+
+    def handlePolygonMessage(self, msg, type="eye"):
+        points = msg.points
+
+        if len(points) <= 0: return 
+
+        currentVI = points[0].view_influence
+        seqNumber = 0
+
+        pointsToPublish = []
+        
+        for p in points:
+            if (p.view_influence == currentVI):
+                pointsToPublish.append(p)
+            else:
+                self.publishPoints(points=pointsToPublish, viewInfluence=currentVI, 
+                                    seqNumber=seqNumber, finalSeq=False, type=type )
+                pointsToPublish.clear()
+                seqNumber += 1
+
+                pointsToPublish.append(p)
+                currentVI = p.view_influence
+        
+        self.publishPoints(points=pointsToPublish, viewInfluence=currentVI, 
+                        seqNumber=seqNumber, finalSeq=True, type=type )
+
+
+    def onSetEye(self, msg):
+        self.logger.info("Set {} eye --> points: {}".format(self.side, msg.points))
+        self.handlePolygonMessage(msg, type = "eye")
+
+
+    def onSetIris(self, msg):
+        self.logger.info("Set {} iris --> points: {}".format(self.side, msg.points))
+        self.handlePolygonMessage(msg, type = "iris")
+
+    def onSetFill(self, msg):
+        self.fill = msg.data
+        self.logger.info("Set {} polygon fill --> {}".format(self.side, self.fill))
+        self.publish_i2c_array(Commands.CMD_FILL_POLYGON, [0x01 if self.fill else 0x00])
+
+    def onShutdown(self):
+        pass
+
+  # def onSetPixel(self, msg):
+  #   self.get_logger().info("Set new pixel on {} display: {}, {}, {}".format(self.side, msg.r, msg.c, msg.v))
+
+  #   o = I2CwriteArray()
+  #   o.address = self.arduinoI2C
+  #   o.command = 0x71
+  #   o.data = [int(msg.r), int(msg.c), int(msg.v)]
+
+  #   self.pubI2CwriteArray.publish(o)
+
+
+    # def onClearDisplay(self, msg):
+    #     self.get_logger().info("Clear {} display: {}".format(self.side, msg.data))
+
+    #     o = I2Cwrite8()
+    #     o.address = self.arduinoI2C
+    #     o.command = 0x70
+    #     o.data = msg.data
+
+    #     self.pubI2Cwrite8.publish(o)
+    #     time.sleep(0.01)
+
 class DisplayControl(Node):
-  def __init__(self, left):
+    def __init__(self):
+        super().__init__('displayControl_node')
 
-    super().__init__('displayControl_node')
+        self.i2cArrayPublisher = self.create_publisher(I2CwriteArray, "system/i2c/writeArray", 10)
 
-    if left: 
-      self.arduinoI2C = 0x0A
-      self.side = "left"
-    else: 
-      self.arduinoI2C = 0x0B
-      self.side = "right"
+        self.nodeLeft = Display(self, i2cArrayPublisher=self.i2cArrayPublisher, side = "left", arduinoAddress = 0x0A)
+        self.nodeRight = Display(self, i2cArrayPublisher=self.i2cArrayPublisher, side = "right", arduinoAddress = 0x0B)
 
-    # Messages for set eye pixel
-    self.create_subscription(DisplayConnectedPixel, "visual/display/setEye", self.onSetEye, 10)
-    self.create_subscription(DisplayConnectedPixel, "visual/display/" + self.side + "/setEye", self.onSetEye, 10)
 
-    # Messages for set iris pixel
-    self.create_subscription(DisplayConnectedPixel, "visual/display/setIris", self.onSetIris, 10)
-    self.create_subscription(DisplayConnectedPixel, "visual/display/" + self.side + "/setIris", self.onSetIris, 10)
-
-    # Messages for Display a single pixel on a Display
-    self.create_subscription(DisplayPixel, "visual/display/setPixel", self.onSetPixel, 10)
-    self.create_subscription(DisplayPixel, "visual/display/" + self.side + "/setPixel", self.onSetPixel, 10)
-
-    # Messages for clearing the displays
-    self.create_subscription(UInt8, "visual/display/clear", self.onClearDisplay, 10)
-    self.create_subscription(UInt8, "visual/display/" + self.side + "/clear", self.onClearDisplay, 10)
-
-    self.pubI2Cwrite8 = self.create_publisher(I2Cwrite8, "system/i2c/write8", 10)
-    self.pubI2Cwrite16 = self.create_publisher(I2Cwrite16, "system/i2c/write16", 10)
-    self.pubI2CwriteArray = self.create_publisher(I2CwriteArray, "system/i2c/writeArray", 10)
-
-    ################################
-    ### LOG INFO ###################
-    self.get_logger().info("################### Init {} display node ################### ".format(self.side))
-    self.get_logger().info("Subscribed: visual/display/setEye        |  Msg: visual/DisplayConnectedPixel")
-    self.get_logger().info("Subscribed: visual/display/{}/setEye  |  Msg: visual/DisplayConnectedPixel".format(self.side))
-
-    self.get_logger().info("Subscribed: visual/display/setIris       |  Msg: visual/DisplayConnectedPixel")
-    self.get_logger().info("Subscribed: visual/display/{}/setIris |  Msg: visual/DisplayConnectedPixel".format(self.side))
-
-    self.get_logger().info("Subscribed: visual/display/setPixel        |  Msg: visual/DisplayPixel")
-    self.get_logger().info("Subscribed: visual/display/{}/setPixel  |  Msg: visual/DisplayPixel".format(self.side))
-
-    self.get_logger().info("Subscribed: visual/display/clear         |  Msg: std_msgs/UInt8")
-    self.get_logger().info("Subscribed: visual/display/{}/clear   |  Msg: std_msgs/UInt8".format(self.side))
-
-    self.get_logger().info("--------------------------------------------------------------------------")
-    self.get_logger().info("Publish on: system/i2c/write8      |  Msg: system/I2Cwrite8")
-    self.get_logger().info("Publish on: system/i2c/write16     |  Msg: system/I2Cwrite16")
-    self.get_logger().info("Publish on: system/i2c/writeArray  |  Msg: system/I2CwriteArray")
-
-    self.get_logger().info("Started {} Display Control Node".format("left" if left else "right"))
-
-  def transmitPixelData(self, msg, cmdControlData, cmdData):
     
-    i2cSem.acquire()
-    
-    time.sleep(0.01)    
-
-    # Transmit the control data
-    o = I2CwriteArray()
-    o.address = self.arduinoI2C
-    o.command = cmdControlData
-    o.data = [int(msg.curve), int(msg.animation), int(msg.value), int(len(msg.points))]
-
-    self.pubI2CwriteArray.publish(o)
-    time.sleep(0.01)
-
-    # Transmit the pixel data
-    o = I2CwriteArray()
-    o.address = self.arduinoI2C
-    o.command = cmdData
-
-    data = []
-
-    for point in msg.points:
-      data.append(int(point.row))
-      data.append(int(point.col))
-
-    o.data = data
-    self.pubI2CwriteArray.publish(o)
-
-    i2cSem.release()
-
-
-
-  def onSetEye(self, msg):
-    self.get_logger().info("Set {} eye --> c: {} | v: {} | a: {} \n{}".format(self.side, msg.curve, msg.value, msg.animation, msg.points))
-
-    self.transmitPixelData(msg, 0x72, 0x73)
-
-
-  def onSetIris(self, msg):
-    self.get_logger().info("Set {} iris --> c: {} | v: {} | a: {} \n{}".format(self.side, msg.curve, msg.value, msg.animation, msg.points))
-
-    self.transmitPixelData(msg, 0x74, 0x75)
-
-
-
-  def onSetPixel(self, msg):
-    self.get_logger().info("Set new pixel on {} display: {}, {}, {}".format(self.side, msg.r, msg.c, msg.v))
-
-    o = I2CwriteArray()
-    o.address = self.arduinoI2C
-    o.command = 0x71
-    o.data = [int(msg.r), int(msg.c), int(msg.v)]
-
-    self.pubI2CwriteArray.publish(o)
-
-
-  def onClearDisplay(self, msg):
-    self.get_logger().info("Clear {} display: {}".format(self.side, msg.data))
-
-    o = I2Cwrite8()
-    o.address = self.arduinoI2C
-    o.command = 0x70
-    o.data = msg.data
-
-    self.pubI2Cwrite8.publish(o)
-    time.sleep(0.01)
+    def onShutdown(self):
+        self.nodeLeft.onShutdown()
+        self.nodeRight.onShutdown()
 
 def main(args=None):
 
-  rclpy.init(args=args) # 'displayControl_node'
+    rclpy.init(args=args) # 'displayControl_node'
 
-  
-  nodeLeft = DisplayControl(True)
-  nodeRight = DisplayControl(False)
+    node = DisplayControl()
 
-
-  rclpy.spin(node)
-  node.onShutdown()
-  node.destroy_node()
-  rclpy.shutdown()
+    rclpy.spin(node)
+    node.onShutdown()
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
   main()
--- ros2/ros_ws/src/visual_py/visual_py/eyeControl.py
+++ ros2/ros_ws/src/visual_py/visual_py/eyeControl.py
@@ -14,7 +14,7 @@ from rclpy.node import Node
 
 from std_msgs.msg import String, Float32, UInt8, Int16
 from systemcore.msg import I2Cwrite8, I2Cwrite16, I2CwriteArray 
-from visual.msg import DisplayConnectedPixel, Point
+from visual.msg import Polygon, PolygonPoint # DisplayConnectedPixel, Point
 
 
 class Colors():
@@ -25,14 +25,16 @@ class Colors():
 
 
 
-class PolygonControl(Node):
+class PolygonControl():
   """ 
   For each Polygon (e.g. Iris and Eye are 2 different polygons) create a PolygonControl object 
   """
 
-  def __init__(self, name, side, setByNameTopic, displayControlTopic, lastTopic, color):
+  def __init__(self, parentNode, name, side, setByNameTopic, displayControlTopic, lastTopic, color, defaultPolygon = None):
 
-    super().__init__('eyeControl_node_{}'.format(name))
+    self.name = 'eyeControl_node_{}'.format(name)
+    self.parentNode = parentNode
+    self.logger = self.parentNode.get_logger()
 
     self.r = redis.Redis(host="localhost", port=6379, db=0)
     self.p = self.r.pubsub(ignore_subscribe_messages=True)
@@ -44,12 +46,16 @@ class PolygonControl(Node):
     self.side = side
 
     ### Polygon set topics subscriber
-    self.create_subscription(String, setByNameTopic, self.onSetByName, 10)
+    self.parentNode.create_subscription(String, setByNameTopic, self.onSetByName, 10)
 
-    self.displayPublisher = self.create_publisher(DisplayConnectedPixel, displayControlTopic, 10)
+    self.displayPublisher = self.parentNode.create_publisher(Polygon, displayControlTopic, 10)
+
+    if defaultPolygon is not None:
+      self.publishPolygon(defaultPolygon)
+
 
     
-    self.get_logger().info('Started Eye Control Nodes{}'.format(name))
+    self.logger.info('Started Eye Control Nodes{}'.format(name))
 
   def getPointsFromName(self, name):
     jsonString = self.r.get("eyes/{}/{}".format(name, self.name))
@@ -62,68 +68,90 @@ class PolygonControl(Node):
     
     return jsonData
     
-
+  def publishPolygon(self, polygon):
+    self.displayPublisher.publish(polygon)
   
   def onSetByName(self, msg):
+    pass
 
-    pointData = self.getPointsFromName(msg.data)
-    if pointData == None: pointData = []
+  #   pointData = self.getPointsFromName(msg.data)
+  #   if pointData == None: pointData = []
 
-    self.get_logger().info("Set ney eye {} with name {}".format(self.name, msg.data))
-    self.get_logger().info(msg)
+  #   self.get_logger().info("Set ney eye {} with name {}".format(self.name, msg.data))
+  #   self.get_logger().info(msg)
     
-    # if len(pointData) == 0:
-    #   return
+  #   # if len(pointData) == 0:
+  #   #   return
     
-    d = DisplayConnectedPixel()
+  #   d = DisplayConnectedPixel()
 
-    # TODO: neues DisplayConnectedPixel Forma mit x,y,c in jedem Punkt
+  #   # TODO: neues DisplayConnectedPixel Forma mit x,y,c in jedem Punkt
     
-    self.get_logger().info(pointData)
+  #   self.get_logger().info(pointData)
 
-    points = []
+  #   points = []
 
-    for pd in pointData:
-      p = Point()
-      p.row = pd['y']
-      p.col = pd['x']
-      #p.x
-      #p.y
-      #p.c
-      points.append(p)
+  #   for pd in pointData:
+  #     p = Point()
+  #     p.row = pd['y']
+  #     p.col = pd['x']
+  #     #p.x
+  #     #p.y
+  #     #p.c
+  #     points.append(p)
 
-    d.points = points
-    d.curve = 0     # linear
-    d.value = self.color     # white
-    d.animation = 0
+  #   d.points = points
+  #   d.curve = 0     # linear
+  #   d.value = self.color     # white
+  #   d.animation = 0
 
-    self.displayPublisher.publish(d)
-    self.get_logger().info("Published new {} with name {}".format(self.name, msg.data))
-    self.r.set(self.redisLastTopic, msg.data)
-
-
+  #   self.displayPublisher.publish(d)
+  #   self.get_logger().info("Published new {} with name {}".format(self.name, msg.data))
+  #   self.r.set(self.redisLastTopic, msg.data)
 
 
-class EyesControl(object):
+
+
+class EyesControl(Node):
   def __init__(self):
+    super().__init__('eyeControl_node')
 
+    defaultEyePoints = [
+      PolygonPoint(x= 10, y= 10, c= -1, cx= 30, cy= 5, view_influence= 50),
+      PolygonPoint(x= 50, y= 10, c= 0, cx= 0, cy= 0, view_influence= 50),
+      PolygonPoint(x= 50, y= 80, c= -1, cx= 30, cy= 90, view_influence= 50),
+      PolygonPoint(x= 10, y= 80, c= 0, cx= 0, cy= 0, view_influence= 50)
+    ]
 
-    leftEye = PolygonControl(name="leftEye",side="left", 
+    defaultIrisPoints = [
+      PolygonPoint(x= 20, y= 30, c= 0, cx= 0, cy= 0, view_influence= 100),
+      PolygonPoint(x= 40, y= 30, c= 0, cx= 0, cy= 0, view_influence= 100),
+      PolygonPoint(x= 40, y= 60, c= 0, cx= 0, cy= 0, view_influence= 100),
+      PolygonPoint(x= 20, y= 60, c= 0, cx= 0, cy= 0, view_influence= 100),
+    ]
+
+    defaultIris = Polygon()
+    defaultIris.points = defaultIrisPoints
+
+    defaultEye = Polygon()
+    defaultEye.points = defaultEyePoints
+
+    leftEye = PolygonControl(parentNode = self, name="leftEye",side="left", defaultPolygon=defaultEye,
       setByNameTopic="eyes/left/setEye/byName", displayControlTopic="visual/display/left/setEye",
       lastTopic="eyes/left/lastEye",
       color=Colors.WHITE)
 
-    rightEye = PolygonControl(name="rightEye",side="right", 
+    rightEye = PolygonControl(parentNode = self, name="rightEye",side="right", defaultPolygon=defaultEye, 
       setByNameTopic="eyes/right/setEye/byName", displayControlTopic="visual/display/right/setEye",
       lastTopic="eyes/right/lastEye",
       color=Colors.WHITE)
 
-    leftIris = PolygonControl(name="leftIris",side="left", 
+    leftIris = PolygonControl(parentNode = self, name="leftIris",side="left", defaultPolygon=defaultIris, 
       setByNameTopic="eyes/left/setIris/byName", displayControlTopic="visual/display/left/setIris",
       lastTopic="eyes/left/lastIris",
       color=Colors.BLACK)
 
-    rightIris = PolygonControl(name="rightIris",side="right", 
+    rightIris = PolygonControl(parentNode = self, name="rightIris",side="right", defaultPolygon=defaultIris,
       setByNameTopic="eyes/right/setIris/byName", displayControlTopic="visual/display/right/setIris",
       lastTopic="eyes/right/lastIris",
       color=Colors.BLACK)
@@ -137,7 +165,6 @@ def main(args=None):
   # TODO: Hier müssen iwie alle Knoten gespint werden
   node = EyesControl()
   
-
   rclpy.spin(node)
   node.onShutdown()
   node.destroy_node()
