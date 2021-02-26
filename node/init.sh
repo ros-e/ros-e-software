80b6a45664aa8b488d0746c67efad31c48908852
Johannes Sommerfeldt <joso2155@th-wildau.de>
2021-02-26 15:49:04 +0000

    included node tool api and frontend in pm2 and nginx setup

--- node/init.sh
+++ node/init.sh
@@ -10,6 +10,10 @@ npm install pm2 -g
 # Install node packages
 npm install /home/rose/software/node/rose-api/ 
 npm install /home/rose/software/node/rose-frontend/ 
+npm install /home/rose/software/node/node-tool-api/
+npm install /home/rose/software/node/node-tool-frontend/ 
+
+
 
 echo "###########################################################################################"
 echo "Start node apps"
@@ -18,5 +22,8 @@ echo "##########################################################################
 # Start services
 cd /home/rose/software/node/rose-api/ && pm2 start api.js --watch -n rose-api
 cd /home/rose/software/node/rose-frontend/ && pm2 start app.js --watch -n rose-frontend
+cd /home/rose/software/node/node-tool-api/ && pm2 start api.js -n node-tool-api
+cd /home/rose/software/node/node-tool-frontend/ && pm2 start app.js -n node-tool-frontend
+
 pm2 save
 
